9df683a19cbef81646fc506b3461cc194beb7667
merlos <404446+merlos@users.noreply.github.com>
2023-11-28 14:14:01 +0300

    use the helm repo instead of the github repo.

--- dev-scripts/update_helm_charts.sh
+++ dev-scripts/update_helm_charts.sh
@@ -17,63 +17,92 @@
 #     helm/README.md
 #
 #
-
-DAGSTER_TAG="1.5.6"
-DASKHUB_TAG="main" # Tags not up to date...
-SUPERSET_TAG="superset-helm-chart-0.10.14"
-
 # Stop the script if there is any error
 set -e
 
 # Displays the commands that are executed. Useful for debug
 set -x
 
-# Create temporary folders
-temp_clone=../.base_repos
+# Define a temporary folder to keep all charts
 temp_charts=$(mktemp -d)
 
-# Location where the new files are copied:
-script_dir=$(dirname "$(realpath "$0")")
-default_output="$script_dir"/../helm/
-
-#TODO add command line option to select output directory
-output_dir=$default_output
-
-# Clones or updates a local repo.
-# If the repo exists in the target_folder(3), it updates it with the tag_name (1)
-# If the repo does not exist, it clones the repo from the repository_url(2)
-clone_or_update_repo() {
-    local tag_name=$1
-    local repository_url=$2
-    local target_folder=$3
-
-    if [ -d "$target_folder/.git" ]; then
-        echo "Updating existing repository $repository_url in $target_folder to tag $tag_name"
-        (cd "$target_folder" && git fetch && git checkout "$tag_name")
-    else
-        echo "Cloning repository $repository_url to $target_folder with tag $tag_name"
-        git clone --branch "$tag_name" --single-branch "$repository_url" "$target_folder"
-    fi
+#
+# Pulls from a repo a chart of a particular version within a specific folder
+#
+helm_pull() {
+    # URL of the helm repo to pull the chart
+    local repo_url=$1
+    # How we're calling the repo locally
+    local repo_name=$2
+    # Name of the cahrt to download
+    local chart_name=$3
+    # Version of the chart to download
+    local chart_version=$4
+    # Folder in which the repo will be downloaded
+    local output_dir=$5
+    
+    echo "Adding helm repo $repo_name ($repor_url)..."
+    helm repo add $repo_name $repo_url
+    echo Updating helm repo $repo_name...
+    helm repo update $repo_name
+    echo "Pulling chart $repo_name/$chart_name:$chart_version..."
+    helm pull  $repo_name/$chart_name --version $chart_version --untar --untardir $output_dir
 }
 
-                    # Branch       # Repo url                             # Dest folder to update/clone
-clone_or_update_repo $DAGSTER_TAG  https://github.com/dagster-io/dagster/ $temp_clone/dagster
-clone_or_update_repo $DASKHUB_TAG  https://github.com/dask/helm-chart     $temp_clone/daskhub
-clone_or_update_repo $SUPERSET_TAG https://github.com/apache/superset     $temp_clone/superset
+#
+# Dagster
+#
+# To get the latest version number use the tag in the github repo
+# https://github.com/dagster-io/dagster/releases Example: 1.5.9
+helm_pull https://dagster-io.github.io/helm \
+        dagster \
+        dagster \
+        1.5.9   \
+        $temp_charts          
 
-#git clone https://github.com/dask/helm-chart $temp_clone/daskhub
-#git clone https://github.com/apache/superset $temp_clone/superset
-#git clone --branch=DAGSTER_TAG https://github.com/dagster-io/dagster/ $temp_clone/dagster
+#
+# Daskhub
+#
+# To get the version number use the release tag in the repo
+# https://github.com/dask/helm-chart/tags. Example 2023.1.0
+helm_pull https://helm.dask.org/ \
+        dask \
+        daskhub \
+        2023.01.0 \
+        $temp_charts          
+
+#
+# Apache Drill
+#
+# To get the version Use the latest release tag in the repo
+# https://github.com/magasin-drill/tags
+helm_pull https://unicef.github.io/magasin-drill \
+        drill \
+        drill \
+        0.6.1   \
+        $temp_charts          
 
 
+#
+# Superset 
+#
+# Search for superset-helm-chart version 
+# https://github.com/apache/superset/tags
+helm_pull https://apache.github.io/superset \
+        superset \
+        superset \
+        0.10.15   \
+        $temp_charts          
 
-cp -r $temp_clone/daskhub/daskhub $temp_charts/
-cp -r $temp_clone/superset/helm/superset $temp_charts/
-cp -rp $temp_clone/dagster/helm/dagster $temp_charts/dagster
+# Location where the new files are copied:
+# TODO add command line option to select output directory
+script_dir=$(dirname "$(realpath "$0")")
+default_output_dir="$script_dir"/../helm/
+output_dir=$default_output_dir
 
 rm -rf $output_dir
 cp -rp $temp_charts/ $output_dir
 
 
 # Clean temporary folders
-rm -rf $temp_charts
+#rm -rf $temp_charts
