470d1eba9637cf099aaf98f9375e8e2d2ff558fa
merlos <404446+merlos@users.noreply.github.com>
2023-12-22 16:42:47 +0300

    remove version from kubectl check. If there is no cluster it fails.

--- scripts/installer/install-magasin.sh
+++ scripts/installer/install-magasin.sh
@@ -382,8 +382,7 @@ not_working=false
 
 echo ""
 echo_info "Verifying commands are working..."
-kubectl version
-if ! kubectl version &> /dev/null; then
+if ! kubectl &> /dev/null; then
   echo_error "The kubectl command ($(command -v "kubectl")) is not working properly."
   echo_error "Installation documentation:"
   echo_error "  - For Linux: https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/"
