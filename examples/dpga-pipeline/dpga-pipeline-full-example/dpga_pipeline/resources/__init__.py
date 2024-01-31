c5e33703513adb91ee410e940d382aeda0f897b2
merlos <404446+merlos@users.noreply.github.com>
2024-01-31 17:20:25 +0300

    add full pipeline example

---
+++ examples/dpga-pipeline/dpga-pipeline-full-example/dpga_pipeline/resources/__init__.py
@@ -0,0 +1,6 @@
+"""
+DPGA Pipeline Resources and IOManagers
+"""
+from .dpg_resource import *
+from .minio_resource import *
+from .minio_manager import *
---
+++ examples/dpga-pipeline/dpga-pipeline-full-example/dpga_pipeline/resources/dpg_resource.py
@@ -0,0 +1,27 @@
+import requests
+from requests import Response
+from pydantic import Field
+
+from dagster import ConfigurableResource
+
+
+class DPGResource(ConfigurableResource):
+    """
+    Annotation
+    """
+    dpga_api_base_url: str = Field(
+        description=(
+            "URL for the DPGA API"
+        ),
+        default="https://api.digitalpublicgoods.net"
+    )
+
+    def get_list_from_dpga(self, stage="dpgs") -> Response:
+        """
+          Calls the dpg API and returns the Response object
+        """ 
+        url = f"{self.dpga_api_base_url}/{stage}"
+        return requests.get(url)
+
+
+
