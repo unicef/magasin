f52eedbe68f205e31acb5205661702e85a5b844d
merlos <404446+merlos@users.noreply.github.com>
2024-06-20 14:31:31 +0300

    doc ports module

--- mag-cli/mag/mag_core/realm.py
+++ mag-cli/mag/mag_core/realm.py
@@ -6,10 +6,19 @@ def split_realm(realm: str) -> tuple:
     Split the realm into prefix and suffix based on the last occurrence of "-".
 
     Args:
-    - realm (str): The input realm string.
+        realm (str): The input realm string.
 
     Returns:
-    - tuple: A tuple containing prefix and suffix.
+        tuple: A tuple containing prefix and suffix.
+
+    Example:
+        split_realm("magasin") -> ("magasin", "")
+        split_realm("magasin-post") -> ("magasin", "post")
+        split_realm("magasin-1-post") -> ("magasin-1", "post")
+        split_realm("dev-magasin-1") -> ("dev-magasin", "1")
+
+    Reference:
+        For more information about magasin realms, please see the [magasin realms documentation](https://unicef.github.io/magasin/install/advanced.html#magasin-realms).
     """
     last_dash_index = realm.rfind("-")
 
@@ -25,6 +34,21 @@ def split_realm(realm: str) -> tuple:
 
 
 def get_namespace(component_name: str, realm='magasin') -> str:
+    """
+    Generate a namespace based on the component name and realm.
+
+
+    Args:
+    - component_name (str): The magasin component name (superset, daskhub, drill, ...)
+    - realm (str, optional): The realm. Defaults to 'magasin'.
+
+    Example:
+        get_namespace("superset", "magasin") -> "magasin-superset"
+        get_namespace("superset", "magasin-postfix") -> "magasin-superset-postfix"
+
+    Reference:
+        For more information about magasin realms, please see the [magasin realms documentation](https://unicef.github.io/magasin/install/advanced.html#magasin-realms).
+    """
     prefix, suffix = split_realm(realm)
     namespace = ""
     if prefix:
